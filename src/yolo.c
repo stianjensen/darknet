58fadce110d77a5cf59b497f3e7a31abc8214858
Stian Jensen <me@stianj.com>
2016-05-02 19:31:20 +0200

    Output intermediate weights more often during training

--- src/yolo.c
+++ src/yolo.c
@@ -72,7 +72,10 @@ void train_yolo(char *cfgfile, char *weightfile)
         avg_loss = avg_loss*.9 + loss*.1;
 
         printf("%d: %f, %f avg, %f rate, %lf seconds, %d images\n", i, loss, avg_loss, get_current_rate(net), sec(clock()-time), i*imgs);
-        if(i%1000==0 || i == 600){
+        if((i % 1000 == 0) ||
+           (i <= 1000 && i % 100 == 0) ||
+           (i <= 2000 && i % 200 == 0) ||
+           (i == 50)) {
             char buff[256];
             sprintf(buff, "%s/%s_%d.weights", backup_directory, base, i);
             save_weights(net, buff);
