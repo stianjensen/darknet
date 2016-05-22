228464c644a1c18cc0374c244330a8f44231647c
Stian Jensen <me@stianj.com>
2016-05-22 23:08:04 +0200

    Search for rightmost dot when removing file extension

--- src/utils.c
+++ src/utils.c
@@ -107,7 +107,7 @@ char *basecfg(char *cfgfile)
         c = next+1;
     }
     c = copy_string(c);
-    next = strchr(c, '.');
+    next = strrchr(c, '.');
     if (next) *next = 0;
     return c;
 }
